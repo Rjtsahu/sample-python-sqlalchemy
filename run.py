7519a2121d2a37010c7c5940b98b314db8d1e0d7
Rajat-PC <abhishek2480tambulkar@yahoo.com>
2018-07-07 22:21:51 +0530

    setup dev branch

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from db.db_main import create_database,create_test_record,fetch_all_person
+from db.db_main import fetch_all_person
 
 if __name__ == '__main__':
-    # create_database()
-    # create_test_record()
     fetch_all_person()
